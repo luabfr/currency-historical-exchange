8ff453fbe2073eaa074e48b9ad002545768779e2
luabfr <luabfr@gmail.com>
2021-07-05 18:36:27 -0300

    fix icons

--- src/Components/MyDatePicker/MyDatePicker.jsx
+++ src/Components/MyDatePicker/MyDatePicker.jsx
@@ -14,7 +14,7 @@ const MyDatePicker = ({ label }) => {
 				onChange={(date) => setStartDate(date)} 
 			/>
 			<span className="svg-container">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 9v10a2 2 0 002 2h12a2 2 0 002-2V9M4 9V7a2 2 0 012-2h2M4 9h16m0 0V7a2 2 0 00-2-2h-2m0 0V3m0 2H8m0-2v2" /> </svg>
+				<svg xmlns="http://www.w3.org/2000/svg" width="2.2rem" height="2.2rem" fill="none" viewBox="0 0 24 24"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 9v10a2 2 0 002 2h12a2 2 0 002-2V9M4 9V7a2 2 0 012-2h2M4 9h16m0 0V7a2 2 0 00-2-2h-2m0 0V3m0 2H8m0-2v2" /> </svg>
 			</span>
 		</div>
 	);
